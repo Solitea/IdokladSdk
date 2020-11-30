11e4fa01fb9c79f0bf5580c669949a72f91ad0e8
RadekScholz <63337965+RadekScholz@users.noreply.github.com>
2020-11-30 14:12:29 +0100

    Identification number validation attribute (#61)
    * Identification number validation attribute
    
    * Fix warnings

---
+++ IdokladSdk/Enums/IdentificationValidationResult.cs
@@ -0,0 +1,33 @@
+﻿namespace IdokladSdk.Enums
+{
+    /// <summary>
+    /// IdentificationValidationResult.
+    /// </summary>
+    public enum IdentificationValidationResult
+    {
+        /// <summary>
+        /// Validation OK
+        /// </summary>
+        Ok = 0,
+
+        /// <summary>
+        /// No value to validate
+        /// </summary>
+        NoValue = 1,
+
+        /// <summary>
+        /// Bad format of value
+        /// </summary>
+        BadFormat = 2,
+
+        /// <summary>
+        /// Check validation error
+        /// </summary>
+        CheckFailed = 3,
+
+        /// <summary>
+        /// Other validation error
+        /// </summary>
+        OtherError = 4
+    }
+}
---
+++ IdokladSdk/Validation/Attributes/IdentificationNumberAttribute.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+using IdokladSdk.Enums;
+
+namespace IdokladSdk.Validation.Attributes
+{
+    /// <summary>
+    /// IdentificationNumberAttribute.
+    /// </summary>
+    public class IdentificationNumberAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var identNumber = Convert.ToString(value, CultureInfo.InvariantCulture);
+            if (string.IsNullOrEmpty(identNumber))
+            {
+                return ValidationResult.Success;
+            }
+
+            var result = IdentificationNumberHelper.IdentificationNumberValidation(identNumber);
+
+            if (result != IdentificationValidationResult.Ok && result != IdentificationValidationResult.NoValue)
+            {
+                return new ValidationResult("Not valid identification number");
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
