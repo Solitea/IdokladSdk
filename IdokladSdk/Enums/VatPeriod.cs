f7ed79d95a5bd86aa64b31c6055cb2db5a1f97db
Petr Venuta <Petr.Venuta@csw.cz>
2021-05-05 11:35:37 +0200

    Added VatPeriod Enum to AgendaPatchModel

---
+++ IdokladSdk/Enums/VatPeriod.cs
@@ -0,0 +1,18 @@
+﻿namespace IdokladSdk.Enums
+{
+    /// <summary>
+    /// Vat period.
+    /// </summary>
+    public enum VatPeriod
+    {
+        /// <summary>
+        /// Monthly.
+        /// </summary>
+        Month = 1,
+
+        /// <summary>
+        /// Quarterly.
+        /// </summary>
+        Quarter = 3
+    }
+}
