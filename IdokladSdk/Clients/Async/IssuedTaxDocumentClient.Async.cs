800fce21f00452bee8133116f0f86709dac424d0
Miroslav Gašparovič <Miroslav.Gasparovic@csw.cz>
2021-10-19 13:05:21 +0200

    IssuedTaxDocument - new methods Default and Post

--- IdokladSdk/Clients/Async/IssuedTaxDocumentClient.Async.cs
+++ IdokladSdk/Clients/Async/IssuedTaxDocumentClient.Async.cs
@@ -2,12 +2,19 @@
 using System.Threading.Tasks;
 using IdokladSdk.Models.IssuedTaxDocument.Get;
 using IdokladSdk.Models.IssuedTaxDocument.Patch;
+using IdokladSdk.Models.IssuedTaxDocument.Post;
 using IdokladSdk.Response;
 
 namespace IdokladSdk.Clients
 {
     public partial class IssuedTaxDocumentClient
     {
+        /// <inheritdoc />
+        public Task<ApiResult<IssuedTaxDocumentGetModel>> DefaultAsync(int id, CancellationToken cancellationToken = default)
+        {
+            return DefaultAsync<IssuedTaxDocumentGetModel>(id, cancellationToken);
+        }
+
         /// <inheritdoc />
         public Task<ApiResult<bool>> DeleteAsync(int id, CancellationToken cancellationToken = default)
         {
@@ -24,6 +31,12 @@ namespace IdokladSdk.Clients
         {
             var resource = $"{ResourceUrl}/{id}";
             return PostAsync<IssuedTaxDocumentGetModel>(resource, cancellationToken);
+        }
+
+        /// <inheritdoc/>
+        public Task<ApiResult<IssuedTaxDocumentGetModel>> PostAsync(IssuedTaxDocumentPostModel model, CancellationToken cancellationToken = default)
+        {
+            return PostAsync<IssuedTaxDocumentPostModel, IssuedTaxDocumentGetModel>(model, cancellationToken);
         }
 
         /// <inheritdoc/>
---
+++ IdokladSdk/Models/IssuedTaxDocument/Post/IssuedTaxDocumentItemPostModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using IdokladSdk.Enums;
+
+namespace IdokladSdk.Models.IssuedTaxDocument.Post
+{
+    /// <summary>
+    /// IssuedTaxDocumentItemPostModel.
+    /// </summary>
+    public class IssuedTaxDocumentItemPostModel
+    {
+        /// <summary>
+        /// Gets or sets Item name.
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Price type.
+        /// </summary>
+        [Required]
+        public PriceType PriceType { get; set; }
+
+        /// <summary>
+        /// Gets or sets Vat code id.
+        /// </summary>
+        public int? VatCodeId { get; set; }
+
+        /// <summary>
+        /// Gets or sets VAT rate in percent.
+        /// </summary>
+        [Required]
+        public decimal VatRate { get; set; }
+    }
+}
