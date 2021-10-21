3e2e160137a36445facdd9b93a43290b8dadcc9d
miroelk <62104255+miroelk@users.noreply.github.com>
2021-10-21 15:09:22 +0200

    Changed properties of Issued tax document post item (#110)
    * Changed properties of IssuedTaxDocumentId
    
    * Fixed documentation

--- IdokladSdk/Models/IssuedTaxDocument/Post/IssuedTaxDocumentItemPostModel.cs
+++ IdokladSdk/Models/IssuedTaxDocument/Post/IssuedTaxDocumentItemPostModel.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using IdokladSdk.Enums;
 
 namespace IdokladSdk.Models.IssuedTaxDocument.Post
 {
@@ -9,26 +8,20 @@ namespace IdokladSdk.Models.IssuedTaxDocument.Post
     public class IssuedTaxDocumentItemPostModel
     {
         /// <summary>
+        /// Gets or sets Id entity.
+        /// </summary>
+        [Required]
+        public int Id { get; set; }
+
+        /// <summary>
         /// Gets or sets Item name.
         /// </summary>
         [Required]
         public string Name { get; set; }
 
         /// <summary>
-        /// Gets or sets Price type.
-        /// </summary>
-        [Required]
-        public PriceType PriceType { get; set; }
-
-        /// <summary>
         /// Gets or sets Vat code id.
         /// </summary>
         public int? VatCodeId { get; set; }
-
-        /// <summary>
-        /// Gets or sets VAT rate in percent.
-        /// </summary>
-        [Required]
-        public decimal VatRate { get; set; }
     }
 }
