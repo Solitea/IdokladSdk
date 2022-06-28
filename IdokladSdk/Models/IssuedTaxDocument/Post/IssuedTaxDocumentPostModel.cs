bf93affe83816ac98a1ba8d9cfa8333ba43a3591
PVenuta <69293876+PVenuta@users.noreply.github.com>
2022-06-28 09:51:21 +0200

    Added IssuedTaxDocumentPostModel validation (#124)
    Co-authored-by: Petr Venuta <Petr.Venuta@csw.cz>

--- IdokladSdk/Models/IssuedTaxDocument/Post/IssuedTaxDocumentPostModel.cs
+++ IdokladSdk/Models/IssuedTaxDocument/Post/IssuedTaxDocumentPostModel.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using IdokladSdk.Models.Base;
 
 namespace IdokladSdk.Models.IssuedTaxDocument.Post
 {
     /// <summary>
     /// IssuedTaxDocumentPostModel.
     /// </summary>
-    public class IssuedTaxDocumentPostModel
+    public class IssuedTaxDocumentPostModel : ValidatableModel
     {
         /// <summary>
         /// Gets or sets Date of issue.
