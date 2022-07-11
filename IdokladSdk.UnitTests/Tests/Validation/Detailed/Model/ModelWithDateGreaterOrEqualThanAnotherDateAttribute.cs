87e046d5be755abf93693dcc2bb691838ef9bb47
Kalus Jiří, Ing <Jiri.Kalus@csw.cz>
2022-07-11 09:50:56 +0200

    fix SalesOrder DateOfExpiration validation

---
+++ IdokladSdk.UnitTests/Tests/Validation/Detailed/Model/ModelWithDateGreaterOrEqualThanAnotherDateAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using IdokladSdk.Validation.Attributes;
+
+namespace IdokladSdk.UnitTests.Tests.Validation.Detailed.Model
+{
+    public class ModelWithDateGreaterOrEqualThanAnotherDateAttribute
+    {
+        [DateGreaterThanOrEqualThanAnotherDate(nameof(DateOfIssue))]
+        public DateTime DateOfExpiration { get; set; }
+
+        public DateTime DateOfIssue { get; set; }
+    }
+}
