d300a1ad5c48a32ce0613a071ff4c98edadd3755
jirikalus <63059528+jirikalus@users.noreply.github.com>
2022-09-21 12:46:24 +0200

    IdentificationNumber validation - POST (#158)
    Co-authored-by: Kalus Jiří, Ing <Jiri.Kalus@csw.cz>

---
+++ IdokladSdk.UnitTests/Tests/Validation/Detailed/Model/PostModelWithIdentificationNumberAttribute.cs
@@ -0,0 +1,12 @@
+﻿using IdokladSdk.Validation.Attributes;
+
+namespace IdokladSdk.UnitTests.Tests.Validation.Detailed.Model
+{
+    public class PostModelWithIdentificationNumberAttribute
+    {
+        public bool HasNoIdentificationNumber { get; set; }
+
+        [IdentificationNumberPost(nameof(HasNoIdentificationNumber))]
+        public string IdentificationNumber { get; set; }
+    }
+}
