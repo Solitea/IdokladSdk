abc14b98766513a54d0df1de34abf6003958198c
miroelk <62104255+miroelk@users.noreply.github.com>
2022-12-08 11:05:53 +0100

    Added missing property (#171)

--- IdokladSdk/Models/Attachment/DocumentAttachmentInfo.cs
+++ IdokladSdk/Models/Attachment/DocumentAttachmentInfo.cs
@@ -9,5 +9,10 @@
         /// Gets or sets file name.
         /// </summary>
         public string FileName { get; set; }
+
+        /// <summary xml:lang="en">
+        /// Gets or sets entity's Id.
+        /// </summary>
+        public int Id { get; set; }
     }
 }
