06efcca521955ba92b3de1dd4546fb800b74eee1
Petr Venuta <Petr.Venuta@csw.cz>
2023-04-12 12:41:40 +0200

    Fix field documentation.

--- IdokladSdk/Validation/Attributes/DecimalRangeAttribute.cs
+++ IdokladSdk/Validation/Attributes/DecimalRangeAttribute.cs
@@ -5,12 +5,12 @@ namespace IdokladSdk.Validation.Attributes
     public class DecimalRangeAttribute : RangeAttribute
     {
         /// <summary>
-        /// Minimum possible value of SQL decimal(18, 4)
+        /// Minimum possible value of SQL decimal(18, 4).
         /// </summary>
         private const string MinValue = "-99999999999999.9999";
 
         /// <summary>
-        /// Maximum possible value of SQL decimal(18, 4)
+        /// Maximum possible value of SQL decimal(18, 4).
         /// </summary>
         private const string MaxValue = "99999999999999.9999";
 
