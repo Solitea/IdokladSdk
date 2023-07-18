48374a515b02b25f7cfa4e01d3c21223989fadd9
Petr Venuta <Petr.Venuta@csw.cz>
2023-07-18 15:20:32 +0200

    Added password strength validator

---
+++ IdokladSdk/Validation/PasswordStrengthValidator/PasswordStrengthValidationResult.cs
@@ -0,0 +1,13 @@
+﻿namespace IdokladSdk.Validation.PasswordStrengthValidator
+{
+    public class PasswordStrengthValidationResult
+    {
+        public bool HasRequiredLength { get; set; }
+
+        public bool HasNumber { get; set; }
+
+        public bool HasLowerCaseLetter { get; set; }
+
+        public bool HasUpperCaseLetter { get; set; }
+    }
+}
---
+++ IdokladSdk/Validation/PasswordStrengthValidator/PasswordStrengthValidator.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.RegularExpressions;
+
+namespace IdokladSdk.Validation.PasswordStrengthValidator
+{
+    public class PasswordStrengthValidator : IPasswordStrengthValidator
+    {
+        private const string HasLowerCaseLetter = @"(?=.*[a-z])";
+        private const string HasNumber = @"(?=.*\d)";
+        private const string HasUpperCaseLetter = @"(?=.*[A-Z])";
+        private const int RequiredLength = 8;
+
+        public PasswordStrengthValidationResult Validate(string password)
+        {
+            return new PasswordStrengthValidationResult
+            {
+                HasLowerCaseLetter = new Regex(HasLowerCaseLetter).IsMatch(password),
+                HasNumber = new Regex(HasNumber).IsMatch(password),
+                HasUpperCaseLetter = new Regex(HasUpperCaseLetter).IsMatch(password),
+                HasRequiredLength = password.Length >= RequiredLength,
+            };
+        }
+    }
+}
