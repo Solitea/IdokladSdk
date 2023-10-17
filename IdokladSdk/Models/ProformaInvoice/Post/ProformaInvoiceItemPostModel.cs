649442e1ce3d81744ab7e834c0a15226441850ea
PVenuta <69293876+PVenuta@users.noreply.github.com>
2023-10-17 13:04:47 +0200

    Fixed ProformaFromSalesOrder model + ProformaInvoiceItemPost validations (#218)
    Co-authored-by: Petr Venuta <Petr.Venuta@csw.cz>

--- IdokladSdk/Models/ProformaInvoice/Post/ProformaInvoiceItemPostModel.cs
+++ IdokladSdk/Models/ProformaInvoice/Post/ProformaInvoiceItemPostModel.cs
@@ -1,60 +1,75 @@
-﻿using IdokladSdk.Enums;
+﻿using System.ComponentModel.DataAnnotations;
+using IdokladSdk.Enums;
+using IdokladSdk.Models.Base;
+using IdokladSdk.Validation.Attributes;
 
 namespace IdokladSdk.Models.ProformaInvoice
 {
     /// <summary>
     /// ProformaInvoiceItemPostModel.
     /// </summary>
-    public class ProformaInvoiceItemPostModel
+    public class ProformaInvoiceItemPostModel : ValidatableModel
     {
         /// <summary>
         /// Gets or sets item amount.
         /// </summary>
+        [Required]
+        [DecimalRange]
         public decimal Amount { get; set; }
 
         /// <summary>
         /// Gets or sets item code.
         /// </summary>
+        [StringLength(20)]
         public string Code { get; set; }
 
         /// <summary>
         /// Gets or sets a value indicating whether is the item a tax movement indication.
         /// </summary>
+        [Required]
         public bool IsTaxMovement { get; set; }
 
         /// <summary>
         /// Gets or sets item name.
         /// </summary>
+        [Required(AllowEmptyStrings = false)]
+        [StringLength(200)]
         public string Name { get; set; }
 
         /// <summary>
         /// Gets or sets pricelist item id.
         /// </summary>
+        [NullableForeignKey]
         public int? PriceListItemId { get; set; }
 
         /// <summary>
         /// Gets or sets price type.
         /// </summary>
+        [Required]
         public PriceType PriceType { get; set; }
 
         /// <summary>
         /// Gets or sets unit of measure.
         /// </summary>
+        [StringLength(20)]
         public string Unit { get; set; }
 
         /// <summary>
         /// Gets or sets unit price.
         /// </summary>
+        [Required]
         public decimal UnitPrice { get; set; }
 
         /// <summary>
         /// Gets or sets vAT rate type.
         /// </summary>
+        [Required]
         public VatRateType VatRateType { get; set; }
 
         /// <summary>
         /// Gets or sets id členení DPH.
         /// </summary>
+        [NullableForeignKey]
         public int? VatCodeId { get; set; }
     }
 }
