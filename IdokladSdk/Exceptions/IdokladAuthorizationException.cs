dcebef8b0f2c96ea2109b6bd2cf8a5529a48bce0
David Novotný <David.Novotny@csw.cz>
2023-10-31 10:22:14 +0100

    Fix Stylecop

--- IdokladSdk/Exceptions/IdokladAuthorizationException.cs
+++ IdokladSdk/Exceptions/IdokladAuthorizationException.cs
@@ -1,17 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Net.Http;
-using System.Text;
+﻿using System.Net.Http;
 
 namespace IdokladSdk.Exceptions
 {
+    /// <summary>
+    /// IdokladAuthorizationException.
+    /// </summary>
     public class IdokladAuthorizationException : IdokladBaseException
     {
         /// <summary>
         /// Initializes a new instance of the <see cref="IdokladAuthorizationException"/> class.
         /// </summary>
         /// <param name="response">RestResponse.</param>
-        public IdokladAuthorizationException(HttpResponseMessage response) : base("User is not authorized")
+        public IdokladAuthorizationException(HttpResponseMessage response)
+            : base("User is not authorized")
         {
             Response = response;
         }
