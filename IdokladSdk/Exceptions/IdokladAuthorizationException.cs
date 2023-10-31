045a17ab2f586f4251b87c3164e821788be0f692
Filip Leifer <filipleifer@gmail.com>
2023-10-31 10:02:05 +0100

    Task 177983: Reagovat na úpravy v identity serveru

---
+++ IdokladSdk/Exceptions/IdokladAuthorizationException.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace IdokladSdk.Exceptions
+{
+    public class IdokladAuthorizationException : IdokladBaseException
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="IdokladAuthorizationException"/> class.
+        /// </summary>
+        /// <param name="response">RestResponse.</param>
+        public IdokladAuthorizationException(HttpResponseMessage response) : base("User is not authorized")
+        {
+            Response = response;
+        }
+
+        /// <summary>
+        /// Gets response.
+        /// </summary>
+        public HttpResponseMessage Response { get; private set; }
+    }
+}
