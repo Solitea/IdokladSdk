3ea5df30d10347e59a34bf469f12b43614b1dbbb
DSpacil <144335717+DSpacil@users.noreply.github.com>
2023-12-05 08:42:33 +0100

    Added BankAccountNumberAttribute unit tests (#226)

---
+++ IdokladSdk.UnitTests/Tests/Validation/Detailed/Model/ModelWithBankAccountNumberAttribute.cs
@@ -0,0 +1,10 @@
+﻿using IdokladSdk.Validation.Attributes;
+
+namespace IdokladSdk.UnitTests.Tests.Validation.Detailed.Model
+{
+    public class ModelWithBankAccountNumberAttribute
+    {
+        [BankAccountNumber]
+        public string BankAccountNumber { get; set; }
+    }
+}
