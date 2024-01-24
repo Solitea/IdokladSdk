a632396f13000e78d1586038b53e893db0b6e4bd
PVenuta <69293876+PVenuta@users.noreply.github.com>
2024-01-24 15:23:14 +0100

    Added statistics methods. (#239)
    * Added statistics methods.
    
    * Changed VatPayerProgress test according to code review comment
    
    ---------
    
    Co-authored-by: Petr Venuta <Petr.Venuta@csw.cz>

---
+++ IdokladSdk/Models/Statistics/TopDebtorGetModel.cs
@@ -0,0 +1,23 @@
+﻿namespace IdokladSdk.Models.Statistics
+{
+    /// <summary>
+    /// Contacts with highest total debt.
+    /// </summary>
+    public class TopDebtorGetModel
+    {
+        /// <summary>
+        /// Gets or sets contact name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Partner contact id.
+        /// </summary>
+        public int PartnerContactId { get; set; }
+
+        /// <summary>
+        /// Gets or sets total invoice sum with VAT in home currency.
+        /// </summary>
+        public decimal TotalWithVatHc { get; set; }
+    }
+}
---
+++ IdokladSdk/Models/Statistics/VatPayerProgressGetModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IdokladSdk.Models.Statistics
+{
+    /// <summary>
+    /// Model contains information about VAT payer progress and its limits.
+    /// </summary>
+    public class VatPayerProgressGetModel
+    {
+        /// <summary>
+        /// Gets or sets start of the auditing period.
+        /// </summary>
+        public DateTime DateFrom { get; set; }
+
+        /// <summary>
+        /// Gets or sets end of the auditing period.
+        /// </summary>
+        public DateTime DateTo { get; set; }
+
+        /// <summary>
+        /// Gets or sets total sale value for the auditing period.
+        /// </summary>
+        public decimal TotalSaleHc { get; set; }
+
+        /// <summary>
+        /// Gets or sets vat payer limit.
+        /// </summary>
+        public decimal VatPayerLimit { get; set; }
+    }
+}
