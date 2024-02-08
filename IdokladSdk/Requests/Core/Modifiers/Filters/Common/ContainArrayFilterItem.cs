e5807474f7abce7dab0762d111f236795676f404
Miroslav Gašparovič <Miroslav.Gasparovic@csw.cz>
2024-02-08 12:14:40 +0100

    Reworked contains filter

---
+++ IdokladSdk/Requests/Core/Modifiers/Filters/Common/ContainArrayFilterItem.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+namespace IdokladSdk.Requests.Core.Modifiers.Filters.Common
+{
+    /// <summary>
+    /// Contain array filter item.
+    /// </summary>
+    public class ContainArrayFilterItem : FilterItem<int>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ContainArrayFilterItem"/> class.
+        /// </summary>
+        /// <param name="name">Property name.</param>
+        public ContainArrayFilterItem(string name)
+            : base(name)
+        {
+        }
+
+        /// <summary>
+        /// Item contains given id.
+        /// </summary>
+        /// <param name="id">Id to find.</param>
+        /// <returns>Filter expression.</returns>
+        public FilterExpression Contains(int id)
+        {
+            return new FilterExpression(Name, FilterOperator.Ct, id);
+        }
+
+        /// <summary>
+        /// Item contains given ids.
+        /// </summary>
+        /// <param name="ids">Ids to find.</param>
+        /// <returns>Filter expression.</returns>
+        public FilterExpression Contains(IEnumerable<int> ids)
+        {
+            return new FilterExpression(Name, FilterOperator.Ct, string.Join(",", ids));
+        }
+    }
+}
