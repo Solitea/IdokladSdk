bda0b39b6c0c48d0669386662ac05dcb437bcc9f
Scholz Radek <Radek.Scholz@csw.cz>
2024-07-29 16:54:40 +0200

    Added bank mail notification

---
+++ IdokladSdk/Models/BankStatement/Get/BankMailNotificationHistoryGetModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using IdokladSdk.Enums;
+
+namespace IdokladSdk.Models.BankStatement
+{
+    /// <summary>
+    /// BankMailNotificationHistoryGetModel.
+    /// </summary>
+    public class BankMailNotificationHistoryGetModel
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        public long Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets bank mail history id.
+        /// </summary>
+        public int BankMailHistoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets bank statement id.
+        /// </summary>
+        public int? BankStatementId { get; set; }
+
+        /// <summary>
+        /// Gets or sets date created.
+        /// </summary>
+        public DateTime DateCreated { get; set; }
+
+        /// <summary>
+        /// Gets or sets from.
+        /// </summary>
+        public string From { get; set; }
+
+        /// <summary>
+        /// Gets or sets bank code.
+        /// </summary>
+        public string BankCode { get; set; }
+
+        /// <summary>
+        /// Gets or sets status.
+        /// </summary>
+        public BankMailHistoryStatus Status { get; set; }
+
+        /// <summary>
+        /// Gets or sets bank pairing status.
+        /// </summary>
+        public BankStatementPairingStatus? BankPairingStatus { get; set; }
+
+        /// <summary>
+        /// Gets or sets paid amount.
+        /// </summary>
+        public decimal? PaidAmount { get; set; }
+
+        /// <summary>
+        /// Gets or sets currency id.
+        /// </summary>
+        public int? CurrencyId { get; set; }
+
+        /// <summary>
+        /// Gets or sets currency symbol.
+        /// </summary>
+        public string CurrencySymbol { get; set; }
+    }
+}
