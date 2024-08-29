dde95f686e9679584455a08129fdbb8c51ae5dc3
Spáčil Daniel <Daniel.Spacil@csw.cz>
2024-08-29 10:20:27 +0200

    Fixed documentation on UnpairedDocumentList

--- IdokladSdk/Clients/UnpairedDocumentClient.cs
+++ IdokladSdk/Clients/UnpairedDocumentClient.cs
@@ -1,4 +1,6 @@
-﻿using IdokladSdk.Enums;
+﻿using System.Threading;
+using IdokladSdk.Enums;
+using IdokladSdk.Requests.Core;
 using IdokladSdk.Requests.UnpairedDocument;
 
 namespace IdokladSdk.Clients
@@ -20,7 +22,12 @@ namespace IdokladSdk.Clients
         /// <inheritdoc />
         public override string ResourceUrl => "/UnpairedDocument";
 
-        /// <inheritdoc />
+        /// <summary>
+        /// List of unpaired document. Returns an instance which allows operations on a list of documents (filtering, sorting, etc.)
+        /// and retrieving data by calling <see cref="BaseList{TList,TClient,TGetModel,TFilter,TSort}.GetAsync(CancellationToken)"/>
+        /// </summary>
+        /// <param name="movementType">Movement type.</param>
+        /// <returns>Instance of descendant of <see cref="BaseList{TList,TClient,TGetModel,TFilter,TSort}"/>.</returns>
         public UnpairedDocumentList List(MovementType movementType)
         {
             return new UnpairedDocumentList(this, movementType);
