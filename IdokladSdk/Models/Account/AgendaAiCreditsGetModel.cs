36ca1610197494328b6b637230406f36b97f7c9f
Robin.Honig <robin.honig@seyfor.com>
2026-04-22 16:36:36 +0200

    #218433 ai credits for agenda

---
+++ IdokladSdk/Models/Account/AgendaAiCreditsGetModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IdokladSdk.Models.Account
+{
+    /// <summary>
+    /// AgendaAiCreditsGetModel.
+    /// </summary>
+    public class AgendaAiCreditsGetModel
+    {
+        /// <summary>
+        /// Gets or sets actual number of credits.
+        /// </summary>
+        public int AiCreditCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets actual number of credits agenda obtained when inbox was enabled.
+        /// </summary>
+        public int StartAiCreditCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets number of credits to be expired within next month.
+        /// </summary>
+        public int ExpiringAiCreditCount { get; set; }
+
+        /// <summary>
+        /// Gets or sets expiration date of credits.
+        /// </summary>
+        public DateTime DateExpiringAiCredit { get; set; }
+    }
+}
