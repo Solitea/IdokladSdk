dc1e0973e0b027c02c9793993233b380200194b1
Robin.Honig <robin.honig@seyfor.com>
2026-06-08 08:52:19 +0200

    #220278 vatrate on IssuedInvoiceItemCopyGetModel

--- IdokladSdk/Models/IssuedInvoice/Copy/IssuedInvoiceItemCopyGetModel.cs
+++ IdokladSdk/Models/IssuedInvoice/Copy/IssuedInvoiceItemCopyGetModel.cs
@@ -68,6 +68,11 @@ namespace IdokladSdk.Models.IssuedInvoice
         public int? VatCodeId { get; set; }
 
         /// <summary>
+        /// Gets or sets Vat Rate in percent
+        /// </summary>
+        public decimal VatRate { get; set; }
+
+        /// <summary>
         /// Gets or sets vAT rate type.
         /// </summary>
         public VatRateType VatRateType { get; set; }
